62e94a000acb056be4ccc3e35c99a64f3fc8d1e3
0xBriz <chaindevone@gmail.com>
2022-06-21 18:54:30 -0400

    time auths

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const WETH = ""; // WBNB
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  // Needs deployment also:
+  // - ProtocolFeesCollector
+  // - BalancerHelpers
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  // constructor(
+  //   IAuthorizer authorizer, // TimelockAuthorizer,
+  //   IWETH weth,
+  //   uint256 pauseWindowDuration,
+  //   uint256 bufferPeriodDuration
+  // )
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await Vault.deploy();
+  await vault.deployed();
+  console.log("Vault deployed to: ", vault.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
