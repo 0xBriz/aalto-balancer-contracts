cdd6cdfa9add9b578249ee164ae64971cd9f907e
0xBriz <chaindevone@gmail.com>
2022-06-29 10:59:05 -0400

    successfully test deploying 3 pool

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,48 @@
 import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { WeightedPoolFactory__factory } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const VAULT_ADDRESS = "0x3B415b38f1c2aE9Af2D1e04F30188AD7dE883B7a";
+const TIME_AUTH = "0x68a3e75a68aD5b8ED2C59a63b95F39C5D3F8cE71";
+const WEIGHTED_FACTORY = "0xbbB3Abfa2dd320d85c64e8825c1E32ad0026fAe5";
+const AMES_ADDRESS = "0xb9E05B4C168B56F73940980aE6EF366354357009";
+const BUSD_ADDRESS = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+const USDC_ADDRESS = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
+const ZERO_ADDRESS = ethers.constants.AddressZero;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Greeter", () => {
+  it("Should deploy a pool", async () => {
+    const NAME = "Test Pool";
+    const SYMBOL = "50AMES-25BUSD-25USDC";
+    const swapFeePercentage = 0.005e18; // 0.5%
+    const tokens = [USDC_ADDRESS, AMES_ADDRESS, BUSD_ADDRESS]; // Must be in sorted order
+    const weights = [parseUnits("0.5"), parseUnits("0.25"), parseUnits("0.25")];
+    const assetManagers = [ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS];
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const owner = (await ethers.getSigners())[0];
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const factory = await ethers.getContractAt(
+      "WeightedPoolFactory",
+      WEIGHTED_FACTORY
+    );
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const tx = await factory.create(
+      NAME,
+      SYMBOL,
+      tokens,
+      weights,
+      assetManagers,
+      swapFeePercentage,
+      owner.address
+    );
+
+    const receipt = await tx.wait();
+
+    const events = receipt.events.filter((e) => e.event === "PoolCreated");
+    const poolAddress = events[0].args.pool;
+    const pool = await ethers.getContractAt("WeightedPool", poolAddress);
+    const poolId = await pool.getPoolId();
+    console.log(poolId);
   });
 });
