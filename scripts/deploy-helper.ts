c20aa12f3afdc3949b3b0f5fe2f177d1e5308c83
0xBriz <chaindevone@gmail.com>
2022-07-02 16:38:00 -0400

    helpers deployment

---
+++ scripts/deploy-helper.ts
@@ -0,0 +1,14 @@
+import { ethers } from "hardhat";
+import { MAINNET_VAULT } from "./addresses";
+
+async function main() {
+  const BalancerHelpers = await ethers.getContractFactory("BalancerHelpers");
+  const helper = await BalancerHelpers.deploy(MAINNET_VAULT);
+  await helper.deployed();
+  console.log("BalancerHelpers deployed to: ", helper.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
