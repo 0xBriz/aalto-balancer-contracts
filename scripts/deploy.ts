e4e52003b4e7401545d0319067a361de7c4a4881
0xBriz <chaindevone@gmail.com>
2022-07-22 16:27:17 -0400

    deployments

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,15 @@
 import { ethers } from "hardhat";
 import { MAINNET_VAULT } from "./addresses";
+import { deployToken } from "./deploy-token";
 import { deployBootstrapPoolFactory } from "./utils/factories/lbp-factory";
 import { deployStablePoolFactory } from "./utils/factories/stable-factory";
 import { deployWeightedFactory } from "./utils/factories/weighted-factory";
 
 async function main() {
-  // await deployStablePoolFactory(MAINNET_VAULT);
-  await deployBootstrapPoolFactory(MAINNET_VAULT);
+  await deployStablePoolFactory("0x26683651C18018b3d6e0754366D145a5CE1b36bc");
+  await deployBootstrapPoolFactory("0x26683651C18018b3d6e0754366D145a5CE1b36bc");
+
+  // await deployToken();
 }
 
 main().catch((error) => {
